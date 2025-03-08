2cec1acf074690aeb278c56e13de65bef7a939ef
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-03-08 16:50:28 -0500

    tailwind initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      Welcome <span>Team 24</span>to AI Prompt Project
+    <div className="w-full h-[100vh] flex items-center justify-center bg-neutral-300">
+      Welcome{" "}
+      <span className="font-bold text-2xl text-blue-950"> Team 24 </span> to AI
+      Prompt Project
     </div>
   );
 }
