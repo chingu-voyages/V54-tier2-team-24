930540c51f4283482b72a11f92b865eeea5c757c
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-03-11 12:58:17 -0400

    add team names and GitHub Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { FaGithub } from "react-icons/fa";
+
 function App() {
   return (
     <div className="flex flex-col min-h-screen bg-neutral-100">
-<main className="flex-1"></main>
-      <footer>this is footer</footer>
+      <main className="flex-1">Main Content</main>
+      <footer>
+        <FaGithub />
+        <div>
+          <p>Carissa Abraham</p>
+          <p>Aaron Goodwin</p>
+          <p>Chris PO</p>
+          <p>Jessica Hackett</p>
+          <p>Matthew Neie</p>
+          <p>Luis Solar</p>
+          <p>Sokuen Ryan</p>
+          <p>Benjamin Corbett</p>
+          <p>Christin Martin</p>
+        </div>
+      </footer>
     </div>
   );
 }
