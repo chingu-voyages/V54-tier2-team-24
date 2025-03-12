255e246a0b403dee49670a6d7821a3b111360176
Christin-paige <christinpmartin@gmail.com>
2025-03-12 18:50:54 -0400

    additional changes to package-lock.json and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,4 +7,11 @@ function App() {
 
     <div className="w-full h-[100vh] flex items-center justify-center bg-neutral-300">
       Welcome{" "}
-      <span className="font-bold text-2xl text-blue-950"> Team 24 </span> 
+      <span className="font-bold text-2xl text-blue-950"> Team 24 </span> to AI
+      Prompt Project
+    </div>
+    </>
+  );
+}
+
+export default App;
