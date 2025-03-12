f3cf745894e2647c8a7a707138775c7e51082d99
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-03-12 17:27:17 -0400

    footer responsive and complete until final colors are considered

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ function App() {
           href="https://github.com/chingu-voyages/V54-tier2-team-24"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-4xl transition-transform duration-300 hover:scale-110 cursor-pointer text-blue-300"
+          className="text-4xl transition-transform duration-300 hover:scale-110 cursor-pointer text-blue-300 hover:text-blue-500"
         >
           <FaGithub />
         </a>
 
         <p className="text-blue-400">Voyage 54 Team 24</p>
-        <div className="text-sm sm:flex w-3/4 justify-evenly hidden text-blue-400 ">
+        <div className="text-sm md:flex sm:max-w-3xl w-3/4 justify-evenly hidden text-blue-400 sm:items-center ">
           <div className="text-center">
             <p>Carissa Abraham</p>
             <p>Aaron Goodwin</p>
