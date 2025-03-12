9cce194233775b8351f908f7d03ba563acff729b
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-03-12 17:23:24 -0400

    match colors to wireframe

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,37 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen bg-neutral-100">
       <main className="flex-1"> Content</main>
-      <footer className="flex justify-between items-center boder-2 border-neutral-400 h-[7vh] bg-neutral-300 px-2 ">
+      <footer className="flex justify-between items-center boder-2 border-neutral-400 h-[7vh] bg-blue-100 px-2 ">
         <a
           href="https://github.com/chingu-voyages/V54-tier2-team-24"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-4xl transition-transform duration-300 hover:scale-110 cursor-pointer"
+          className="text-4xl transition-transform duration-300 hover:scale-110 cursor-pointer text-blue-300"
         >
           <FaGithub />
         </a>
 
-        <p>Voyage 54 Team 24</p>
-        <div className="text-sm flex w-3/4 justify-evenly ">
-          <div>
+        <p className="text-blue-400">Voyage 54 Team 24</p>
+        <div className="text-sm sm:flex w-3/4 justify-evenly hidden text-blue-400 ">
+          <div className="text-center">
             <p>Carissa Abraham</p>
             <p>Aaron Goodwin</p>
           </div>
+          <div className="text-center">
+            <p>Jessica Hackett</p>
+            <p>Christin Martin </p>
+          </div>
+          <div className="text-center">
+            <p>Matthew Neie</p>
+            <p>Luis Solar</p>
+          </div>
 
-          <p>Chris </p>
-          <p>Jessica Hackett</p>
-          <p>Matthew Neie</p>
-          <p>Luis Solar</p>
-          <p>Sokuen Ryan</p>
-          <p>Benjamin Corbett</p>
-          <p>Christin Martin</p>
+          <div className="text-center">
+            <p>Benjamin Corbett</p>
+            <p>Sokuen Ryan</p>
+          </div>
+
+          <p>Chris</p>
         </div>
       </footer>
     </div>
