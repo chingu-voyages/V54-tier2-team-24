58e69fe3393b3dd8d7cfa1c3d63fc06128580bd0
Christin-paige <christinpmartin@gmail.com>
2025-03-12 18:46:48 -0400

    adding header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import Header from "./components/Header";
+
 function App() {
   return (
+    <>
+    <Header />
+
     <div className="w-full h-[100vh] flex items-center justify-center bg-neutral-300">
       Welcome{" "}
       <span className="font-bold text-2xl text-blue-950"> Team 24 </span> to AI
       Prompt Project
     </div>
+    </>
   );
 }
 
