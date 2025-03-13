b311d6295da45486c4533d7d36175673f8f9222b
Christin-paige <christinpmartin@gmail.com>
2025-03-13 18:41:28 -0400

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import Header from "./components/Header";
+import { GiSpellBook } from "react-icons/gi";
 
 function App() {
   return (
     <>
-    <Header />
+      <header className="flex justify-between items-center p-8 bg-blue-100">
+        <div className="container mx-auto flex items-center justify-between px-4 md:px-8">
+          <GiSpellBook className="text-blue-400 w-16 h-16 sm:w-24 sm:h-24 md:w-32 md:h-32" />
+        </div>
+        <div>
+          <h1 className="text-blue-400 font-sans text-2xl sm:text-3xl md:text-4xl w-full whitespace-nowrap md:pr-20 sm:pr-10">
+            App Name
+          </h1>
+        </div>
+      </header>
 
-    <div className="w-full h-[100vh] flex items-center justify-center bg-neutral-300">
-      Welcome{" "}
-      <span className="font-bold text-2xl text-blue-950"> Team 24 </span> to AI
-      Prompt Project
-    </div>
+      <div className="w-full h-[100vh] flex items-center justify-center">
+        Welcome{" "}
+        <span className="font-bold text-2xl text-blue-950"> Team 24 </span> to
+        AI Prompt Project
+      </div>
     </>
   );
 }
