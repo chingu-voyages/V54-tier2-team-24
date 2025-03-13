f8bed099887b09eb8f64a22f3db08604c283424b
Matthew Neie <matneie@gmail.com>
2025-03-13 14:44:47 -0700

    add loading feature and button to test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import LoadingFeature from "./HandleLoading";
+
 function App() {
   return (
-    <div className="w-full h-[100vh] flex items-center justify-center bg-neutral-300">
+    <div className="w-full h-[100vh] flex flex-col items-center justify-center bg-neutral-300">
       Welcome{" "}
       <span className="font-bold text-2xl text-blue-950"> Team 24 </span> to AI
       Prompt Project
+      <LoadingFeature />
     </div>
   );
 }
