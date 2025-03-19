ec5d7542689b9c60515158602a71b8296c6bbb03
Luis <solarluiso@gmail.com>
2025-03-19 16:37:03 -0400

    enhance CopyButton component with styling and icon

--- src/components/CopyButton.jsx
+++ src/components/CopyButton.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
+import { ClipboardCheck, Check } from "lucide-react";
 
 const CopyButton = ({ text }) => {
   const [copied, setCopied] = useState(false);
 
-  return <button>Copy</button>;
+  return (
+    <button className="flex items-center gap-2 px-6 py-2 rounded-md bg-blue-300 text-blue-500 mt-3 hover:bg-blue-400 hover:text-white transition cursor-pointer">
+      <ClipboardCheck className="w-5 h-5" />
+      Copy
+    </button>
+  );
 };
 
 export default CopyButton;
