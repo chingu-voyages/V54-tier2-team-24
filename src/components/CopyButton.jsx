d216d23c7701d40e32d1637e6a1ebb3d29f26424
Luis <solarluiso@gmail.com>
2025-03-20 18:53:14 -0400

    define the function to copy text and update state and icons

--- src/components/CopyButton.jsx
+++ src/components/CopyButton.jsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
-import { ClipboardCheck, Check } from "lucide-react";
+import React, { useState } from "react";
+import { Copy, Check } from "lucide-react";
 
 const CopyButton = ({ text }) => {
   const [copied, setCopied] = useState(false);
 
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
+
   return (
-    <button className="flex items-center gap-2 px-6 py-2 rounded-md bg-blue-300 text-blue-500 mt-3 hover:bg-blue-400 hover:text-white transition cursor-pointer">
-      <ClipboardCheck className="w-5 h-5" />
-      Copy
+    <button
+      onClick={handleCopy}
+      className="p-1 rounded-md text-blue-500 mt-3 hover:bg-blue-300 transition cursor-pointer"
+    >
+      {copied ? <Check size={16} /> : <Copy size={16} />}
     </button>
   );
 };
