678d2ee52240a2c17820a94a60ca7dfda89b23e5
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-03-27 06:48:39 -0400

    finish help section color / style pending

--- src/components/datatest/HelpTest.jsx
+++
@@ -1,9 +0,0 @@
-import { useFirestore } from "../../contexts/FirestoreContext";
-
-export default function HelpTest() {
-  const { helpDataObject, loading } = useFirestore();
-
-  const faqs = helpDataObject?.faqs;
-  console.log(faqs);
-  return <div>{faqs?.title}</div>;
-}
--- src/components/helpMenu/HelpMenu.jsx
+++ src/components/helpMenu/HelpMenu.jsx
@@ -1,17 +1,30 @@
 import { useEffect, useState } from "react";
-import { useHelpData } from "../../contexts/HelpDataContext";
+import { useFirestore } from "../../contexts/FirestoreContext";
 import AdvanceFeatures from "./helpMenuSections/AdvanceFeatures";
 import Faq from "./helpMenuSections/Faq";
 import GettingStarted from "./helpMenuSections/GettingStarted";
-import { IoCloseOutline } from "react-icons/io5";
 import { FaWindowClose } from "react-icons/fa";
 
 const HelpMenu = ({ width, position, isOpen, onRequestClose }) => {
   const [isVisible, setIsVisible] = useState(isOpen);
   const [isSectionOpen, setIsSectionOpen] = useState(null);
-  const { helpDataObject } = useHelpData();
-  const { introduction, gettingStarted, faqs, advancedFeatures } =
-    helpDataObject;
+  const { helpDataObject, loading } = useFirestore();
+  const getWelcome = (str) => {
+    return str.split(" ").slice(0, 4).join(" ");
+  };
+
+  const removeWelcome = (str, numWords = 4) => {
+    const words = str.split(" ");
+    if (words.length <= numWords) {
+      return "";
+    }
+    return words.slice(numWords).join(" ");
+  };
+
+  const welcome = getWelcome(helpDataObject?.introduction?.content || "");
+  const welcomeContent = removeWelcome(
+    helpDataObject?.introduction?.content || ""
+  );
 
   useEffect(() => {
     if (isOpen) {
@@ -29,6 +42,10 @@ const HelpMenu = ({ width, position, isOpen, onRequestClose }) => {
     setIsSectionOpen((current) => (current === sectionId ? null : sectionId));
   };
 
+  if (loading) {
+    return <p>Loading</p>;
+  }
+
   return (
     <div
       className={`fixed top-0 right-0 bg-black/[0.65] w-full h-screen z-40 transition-opacity duration-300
@@ -43,7 +60,7 @@ const HelpMenu = ({ width, position, isOpen, onRequestClose }) => {
         <div className=" w-full flex flex-col gap-5">
           <div className="flex items-center justify-between px-2 ">
             <h2 className="text-neutral-900  text-2xl font-bold pt-40 sm:pt-40 md:pt-40 underline">
-              {introduction.title}
+              {helpDataObject?.introduction.title}
             </h2>
             <button
               onClick={onRequestClose}
@@ -52,23 +69,25 @@ const HelpMenu = ({ width, position, isOpen, onRequestClose }) => {
               <FaWindowClose />
             </button>
           </div>
-
-          <p>{introduction.content}</p>
+          <div className="text-center flex flex-col gap-3 mb-4">
+            <h2 className="text-center text-3xl font-bold">{welcome}</h2>
+            <p className="mx-1">{welcomeContent}</p>
+          </div>
         </div>
 
-        <div>
+        <div className="flex flex-col justify-evenly items-stretch h-1/2">
           <GettingStarted
-            data={gettingStarted}
+            data={helpDataObject?.gettingStarted}
             toggleSection={toggleSection}
             isSectionOpen={isSectionOpen}
           />
           <Faq
-            data={faqs}
+            data={helpDataObject?.faqs}
             toggleSection={toggleSection}
             isSectionOpen={isSectionOpen}
           />
           <AdvanceFeatures
-            data={advancedFeatures}
+            data={helpDataObject?.advancedFeatures}
             toggleSection={toggleSection}
             isSectionOpen={isSectionOpen}
           />
