3052401a684c3fc955d884e9df2daa0ae0b6b66e
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-03-30 08:41:38 -0400

    finish styling sign in button to match av colors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -85,7 +85,7 @@ function Header() {
         ) : (
           <button
             onClick={handleLogin}
-            className="bg-blue-900 text-white px-4 py-2 rounded"
+            className="bg-blue-300 text-white px-3 py-1 rounded-4xl"
           >
             Google Sign In
           </button>
