1d7a28d3738dbc426c88ab23e61e58e95ca17c5e
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-03-30 08:49:29 -0400

    t wind config and readonly file added for future ref

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          'karla': ['Karla', 'sans-serif'],
-          'inconsolata': ['Inconsolata', 'monospace'],
-        },
-        colors: {
-          'background-start': '#02010B',
-          'background-end': '#0D00A4',
-          'button-icon': '#A3CAF6',
-          'body-dark': '#FFFFFF',
-          'body-light': '#000000',
-        },
-        borderRadius: {
-          'button': '5px',
-          'progress': '8px',
-        },
-      },
-    },
-  }
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//     content: [
+//       "./index.html",
+//       "./src/**/*.{js,ts,jsx,tsx}",
+//     ],
+//     theme: {
+//       extend: {
+//         fontFamily: {
+//           'karla': ['Karla', 'sans-serif'],
+//           'inconsolata': ['Inconsolata', 'monospace'],
+//         },
+//         colors: {
+//           'background-start': '#02010B',
+//           'background-end': '#0D00A4',
+//           'button-icon': '#A3CAF6',
+//           'body-dark': '#FFFFFF',
+//           'body-light': '#000000',
+//         },
+//         borderRadius: {
+//           'button': '5px',
+//           'progress': '8px',
+//         },
+//       },
+//     },
+//   }
