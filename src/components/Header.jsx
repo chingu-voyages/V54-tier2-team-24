d27b64f11ffbe68f370435245ce865f37b871aa1
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-03-30 08:39:45 -0400

    feature complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -47,57 +47,51 @@ function Header() {
   };
 
   return (
-
- 
-
     <header className="flex justify-between items-center sticky top-0 left-0 w-full bg-neutral-900 shadow-md p-4 z-50">
-    {/* Logo and App Name on left */}
-    <div className="flex items-center gap-3">
-      <img
-        src="/designAssets/robot_logo.png"
-        alt="Description of AiQ Logo"
-        className="w-9 h-9"
-      />
-      <h1 className="text-white font-sans text-xl md:text-2xl">
-     AiQ
-      </h1>
-    </div>
-  
-    {/* Current Date in center */}
-    <h3 className="text-white font-sans text-sm sm:text-base">
-      {currentDate}
-    </h3>
-  
-    {/* Auth controls on right */}
-    <div>
-      {isAuthenticated ? (
-        <div className="flex items-center gap-2">
-          <div className="flex items-center">
-            {/* <img
+      <div className="flex items-center gap-3">
+        <img
+          src="/designAssets/robot_logo.png"
+          alt="Description of AiQ Logo"
+          className="w-9 h-9"
+        />
+        <h1 className="text-white font-sans text-xl md:text-2xl">AiQ</h1>
+      </div>
+
+      <h3 className="text-white font-sans text-sm sm:text-base">
+        {currentDate}
+      </h3>
+
+      <div>
+        {isAuthenticated ? (
+          <div className="flex items-center gap-2">
+            <div className="flex items-center">
+              {/* <img
               className="w-10 h-10 rounded-full mr-2"
               src={user?.photoURL}
               alt={`${user.displayName}s Avatar`}
             /> */}
-            <span className="text-white">{user.displayName || "User"}</span>
+              <p className="w-10 h-10 rounded-full mr-2 bg-blue-200 flex items-center justify-center font-bold">
+                A
+              </p>
+              <span className="text-white">{user.displayName || "User"}</span>
+            </div>
+            <button
+              onClick={handleLogout}
+              className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
+            >
+              Sign Out
+            </button>
           </div>
+        ) : (
           <button
-            onClick={handleLogout}
-            className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
+            onClick={handleLogin}
+            className="bg-blue-900 text-white px-4 py-2 rounded"
           >
-            Sign Out
+            Google Sign In
           </button>
-        </div>
-      ) : (
-        <button
-          onClick={handleLogin}
-          className="bg-blue-900 text-white px-4 py-2 rounded"
-        >
-          Google Sign In
-        </button>
-      )}
-    </div>
-  </header>
-
+        )}
+      </div>
+    </header>
   );
 }
 export default Header;
