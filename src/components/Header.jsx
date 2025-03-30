3cb496bf8288c3a46b7469d0d310971c06d4b87e
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-03-30 08:30:11 -0400

    temp remove help menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -48,19 +48,25 @@ function Header() {
 
   return (
     <>
-      <HelpMenu
+      {/* <HelpMenu
         width="w-full sm:w-1/2 md:w-1/3 lg:w-1/3"
         position="right-0"
         isOpen={showHelp}
         onRequestClose={toggleHelp}
-      />
+      /> */}
 
       <header className="flex flex-wrap justify-between items-center sticky top-0 left-0 w-full bg-blue-100 shadow-md p-4 z-50">
         <div className="flex flex-col items-center sm:items-start sm:pl-10 justify-center">
-          <GiSpellBook className="text-blue-400 w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24" />
-          <h1 className="text-blue-400 font-sans text-xl sm:text-lg md:text2xl text-center sm:text-left">
-            App Name
-          </h1>
+          <div className="flex items-center justify-items-start">
+            <img
+              src="/designAssets/robot_logo.png"
+              alt="Description of AiQ Logo"
+              className="w-15 h-15"
+            />
+            <h1 className="text-blue-400 font-sans text-xl sm:text-lg md:text2xl text-center sm:text-left">
+              App Name
+            </h1>
+          </div>
         </div>
 
         <div className="flex flex-col items-end gap-2 pr-5 sm:flex-col sm:items-end sm:ml-auto sm:mt-4">
@@ -96,12 +102,12 @@ function Header() {
           <h3 className="text-blue-400 font-sans mt-6 text-sm sm:text-base">
             {currentDate}
           </h3>
-          <button
+          {/* <button
             className="text-white bg-blue-400 rounded-lg px-2 mt-2 text-sm sm:text-base"
             onClick={toggleHelp}
           >
             Help Menu
-          </button>
+          </button> */}
         </div>
       </header>
     </>
