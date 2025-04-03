bc3b14b181a17f1b7d24c3112f61ba2e8858f2d8
Luis <solarluiso@gmail.com>
2025-04-02 20:32:13 -0400

    implement confirmation toast for resetting all fields in ResetButtons component

--- src/components/ResetButtons.jsx
+++ src/components/ResetButtons.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { RotateCcw, ListRestart } from "lucide-react";
 import { usePentagram } from "./PentagramContext.jsx";
+import { toast } from "react-toastify";
 
 const ResetButtons = ({ field, isResetAll = false }) => {
   const { pentaPrompts, resetField, resetAllFields } = usePentagram();
@@ -15,9 +16,37 @@ const ResetButtons = ({ field, isResetAll = false }) => {
   };
 
   const handleResetAll = () => {
-    if (window.confirm("Are you sure you want to reset all fields?")) {
-      resetAllFields();
-    }
+    toast(
+      ({ closeToast }) => (
+        <div>
+          <p className="mt-1">Are you sure you want to reset all fields?</p>
+          <div className="flex justify-start gap-2">
+            <button
+              onClick={() => {
+                resetAllFields();
+                toast.success("All fields have been reset.");
+                closeToast();
+              }}
+              className="px-4 py-1 rounded-md border-1 border-white-400 text-white-500 mt-3 text-sm shadow-md cursor-pointer"
+            >
+              OK
+            </button>
+            <button
+              onClick={closeToast}
+              className="px-4 py-1 rounded-md border-1 border-white-400 text-white-500 mt-3 text-sm shadow-md cursor-pointer"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      ),
+      {
+        autoClose: 5000,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+      }
+    );
   };
 
   return (
