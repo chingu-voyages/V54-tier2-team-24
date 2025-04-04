5b39fd58a1d8482ddffdc5ef4058ffb1002633a4
Christin-paige <christinpmartin@gmail.com>
2025-04-04 09:27:41 -0400

    added font choice to repsonse

--- src/components/ResponseDisplay.jsx
+++ src/components/ResponseDisplay.jsx
@@ -5,8 +5,8 @@ const ResponseDisplay = ({ responseText })=>{
    
 return (
     <section className="p-5 text-white rounded-lg leading-7">
-        <h1 className="flex justify-center text-lg pb-5 font-bold">Response</h1>
-    <ReactMarkdown children={responseText} /> 
+        <h1 className="flex justify-center text-lg pb-5 font-inconsolata-bold">Response</h1>
+    <ReactMarkdown className="font-inconsolata lg:text-base/9 md:text-[22px]/8 sm:text-base/7" children={responseText} /> 
     </section>
   );
 };
