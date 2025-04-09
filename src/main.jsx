232946029156671d980cf4c6fdf0601e44429a8d
Luis <solarluiso@gmail.com>
2025-04-09 12:52:47 -0400

    remove ErrorBoundary component and its usage in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,11 @@ import "./index.css";
 
 import App from "./App.jsx";
 import { FirestoreProvider } from "./contexts/FirestoreContext.jsx";
-import ErrorBoundary from "./components/ErrorBoundary.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <FirestoreProvider>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
+      <App />
     </FirestoreProvider>
   </StrictMode>
 );
