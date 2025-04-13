ff8837935375fdee1556997dfd396c7fa73472a5
Luis <solarluiso@gmail.com>
2025-04-12 23:33:45 -0400

    change reset confirmation buttons style

--- src/components/ResetButtons.jsx
+++ src/components/ResetButtons.jsx
@@ -27,13 +27,13 @@ const ResetButtons = ({ field, isResetAll = false }) => {
                 toast.success("All fields have been reset.");
                 closeToast();
               }}
-              className="px-4 py-1 rounded-md bg-green-500 text-black mt-3 text-sm shadow-md cursor-pointer"
+              className="px-4 py-1 rounded-md border-1 border-green-400 text-green-400 mt-3 text-sm shadow-md cursor-pointer"
             >
               OK
             </button>
             <button
               onClick={closeToast}
-              className="px-4 py-1 rounded-md bg-red-500 text-black mt-3 text-sm shadow-md cursor-pointer"
+              className="px-4 py-1 rounded-md border-1 border-red-400 text-red-400 mt-3 text-sm shadow-md cursor-pointer"
             >
               Cancel
             </button>
