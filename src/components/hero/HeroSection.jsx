5ed4dd3d9e20696059910ddf7cbde35b495096a7
Matthew Neie <matneie@gmail.com>
2025-04-22 20:20:03 -0700

    test

--- src/components/hero/HeroSection.jsx
+++ src/components/hero/HeroSection.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { auth } from "../../../utils/firebase/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Image1 from "../../images/AI-PNG-File.png";
+import Reveal from "../Reveal";
 import "./HeroSection.css";
 
 const HeroSection = () => {
@@ -23,24 +24,32 @@ const HeroSection = () => {
     <div className="hero-wrapper">
       <div className="hero-container">
         <div className="hero-left">
-          <h1 className="hero-title">Welcome to AiQ</h1>
-          <p className="hero-desc">
-            Powered by Gemini AI, AiQ uses the Pentagram framework to develop
-            specific, meaningful AI responses. <br></br>Here, you will learn
-            about the features of the Pentagram; Persona, Context, Task, Output,
-            Contraints. <br></br>Build your knowledge of AI with AiQ!
-          </p>
-          <div
-            className="get-started-button"
-            onClick={() => {
-              navigate("/pentagram");
-            }}
-          >
-            Get Started
-          </div>
+          <Reveal>
+            <h1 className="hero-title">Welcome to AiQ</h1>
+          </Reveal>
+          <Reveal>
+            <p className="hero-desc">
+              Powered by Gemini AI, AiQ uses the Pentagram framework to develop
+              specific, meaningful AI responses. <br></br>Here, you will learn
+              about the features of the Pentagram; Persona, Context, Task,
+              Output, Contraints. <br></br>Build your knowledge of AI with AiQ!
+            </p>
+          </Reveal>
+          <Reveal>
+            <div
+              className="get-started-button hover:bg-blue-400 cursor-pointer"
+              onClick={() => {
+                navigate("/pentagram");
+              }}
+            >
+              Get Started
+            </div>
+          </Reveal>
         </div>
         <div className="hero-right">
-          <img src={Image1} style={imageStyle} className="hero-img" />
+          <Reveal>
+            <img src={Image1} style={imageStyle} className="hero-img" />
+          </Reveal>
         </div>
       </div>
     </div>
