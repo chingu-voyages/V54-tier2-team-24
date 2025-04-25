ab9685eedb64cc06c2d61e5cdb0fd815765d00e8
Luis <solarluiso@gmail.com>
2025-04-24 21:29:44 -0400

    add gemini temporary logo and response margin bottom

--- src/components/ResponseDisplay.jsx
+++ src/components/ResponseDisplay.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import ReactMarkdown from "react-markdown";
+import { RiGeminiFill } from "react-icons/ri";
 import ExportSinglePrompt from "./ExportSinglePrompt";
 import CopyButton from "./CopyButton";
 import "../index.css";
@@ -9,7 +10,7 @@ const ResponseDisplay = ({ responseText, inputs }) => {
 
   useEffect(() => {
     if (responseEndRef.current) {
-      const headerHeight = window.innerHeight * 0.09; // 7vh - The height of the header + the top margin
+      const headerHeight = window.innerHeight * 0.09;
       const elementPosition =
         responseEndRef.current.getBoundingClientRect().top;
       const offsetPosition = elementPosition + window.scrollY - headerHeight;
@@ -30,11 +31,13 @@ const ResponseDisplay = ({ responseText, inputs }) => {
     <div className="w-full">
       <section
         ref={responseEndRef}
-        className="p-5 text-white rounded-lg leading-7 bg-white/20" // Translucent background and matching width
+        className="p-5 text-white rounded-lg leading-7 bg-white/20 mb-3.5"
       >
-        {/* <h1 className="flex justify-center text-lg pb-5 font-Inconsolata-Bold mt-20">
-          Response
-        </h1> */}
+        <h1 className="flex items-center gap-2 text-lg pb-4 font-Inconsolata-Bold">
+          <RiGeminiFill className="text-white" />
+          Gemini
+        </h1>
+        <hr className="border-white/30 mb-4" />
         <div className="font-Inconsolata-Regular lg:text-base/9 md:text-base/8 sm:text-base/7 space-y-4">
           <ReactMarkdown
             components={{
@@ -55,12 +58,12 @@ const ResponseDisplay = ({ responseText, inputs }) => {
           </div>
 
           <div
-            className="font-inconsolataregular px-4 py-1 bg-blue-300 text-black text-base rounded hover:bg-blue-400 transition cursor-pointer"
+            className="new-prompt-button"
             onClick={() => {
               newPrompt();
             }}
           >
-            + New Prompt
+            New Prompt
           </div>
           <div className="flex justify-center items-center text-center">
             <ExportSinglePrompt responseText={responseText} inputs={inputs} />
