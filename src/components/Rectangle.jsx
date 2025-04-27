d3a6857e1c050d067828986d7906ee4690e10924
Luis <solarluiso@gmail.com>
2025-04-26 20:21:19 -0400

    adjust circle sizes

--- src/components/Rectangle.jsx
+++ src/components/Rectangle.jsx
@@ -1,13 +1,15 @@
-export default function Rectangle({ isFilled,isSelected}) {
-    const borderStyle = isSelected ? "border-double border-white" : "border-solid border-icon";
-    const rectStyle = "w-3 h-3 border-4 rounded-full md:w-8 md:h-8"
-    return (
-        <div
-            className={
-                isFilled
-                    ? `${rectStyle} ${borderStyle} bg-icon `
-                    : `${rectStyle} ${borderStyle}`
-            }
-        />
-    )
+export default function Rectangle({ isFilled, isSelected }) {
+  const borderStyle = isSelected
+    ? "border-double border-white"
+    : "border-solid border-icon";
+  const rectStyle = "w-5 h-5 border-4 rounded-full md:w-7 md:h-7 lg:w-8 lg:h-8";
+  return (
+    <div
+      className={
+        isFilled
+          ? `${rectStyle} ${borderStyle} bg-icon `
+          : `${rectStyle} ${borderStyle}`
+      }
+    />
+  );
 }
